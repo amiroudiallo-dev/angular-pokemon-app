3063f54627d9fd6cb9200cd2469bf0c71e80de96
Amirou DIALLO <amiroudiallo.yw@gmail.com>
2023-03-05 09:52:42 +0000

    |config-project

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {POKEMONS} from "./mock-pokemon-list";
+import {Pokemon} from "./pokemon";
 
 @Component({
   selector: 'app-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
-    <h1>Welcome to {{title}}!</h1>`
+    <h1>Liste des Pokemons</h1>`
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliquez sur le pokemon ${ pokemon.name }`);
+  }
 }
---
+++ ng-pokemon-app/src/app/pokemon.ts
@@ -0,0 +1,17 @@
+/*
+ * @author: Amirou Yannick Wendpuiré DIALLO
+ * @version: 1.0
+ * @email: <A HREF="mailto:amiroudiallo.yw@gmail.com">DIALLO Amirou Y. W</A>
+ * @Since 05/03/2023 09:30
+ * Copyright (c) 2023. amiroudiallo.yw, All rights reserved
+ */
+
+export class Pokemon {
+    id: number;
+    hp: number;
+    cp: number;
+    name: string;
+    picture: string;
+    types: Array<string>;
+    created: Date;
+}
