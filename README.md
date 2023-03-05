a1e1d6148afbcbe2370900ca1d7ffdcd7fb13ff9
Amirou DIALLO <amiroudiallo.yw@gmail.com>
2023-03-05 09:04:29 +0000

    | Project initialazation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # angular-pokemon-app
+### Initialize project
---
+++ ng-pokemon-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <h1>Welcome to {{title}}!</h1>`
+})
+export class AppComponent {
+  title = 'ng-pokemon-app';
+}
